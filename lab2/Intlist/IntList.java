991c8f06ec4562ad1fb19759d4c33c76ed8de0f0
linye0 <2629442736@qq.com>
2023-01-25 21:23:25 +0800

    Completed lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList ptr = A;
+        while(ptr.rest != null) {
+            ptr = ptr.rest;
+        }
+        ptr.rest = B;
+        return A;
     }
 
     /**
@@ -91,6 +96,11 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A != null) {
+            return new IntList(A.first, catenate(A.rest, B));
+        }else if (B != null){
+            return new IntList(B.first, catenate(A, B.rest));
+        }
         return null;
     }
 
