29940767e23561b5e9dced53fe271c642076e983
linye0 <2629442736@qq.com>
2023-01-25 21:45:09 +0800

    Fixed a bug.

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,7 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        if (A == null) return null;
+        if (A == null) return B;
         IntList ptr = A;
         while(ptr.rest != null) {
             ptr = ptr.rest;
