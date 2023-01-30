880c6d637d316f65578c3c7dce27013c686a12e1
linye0 <2629442736@qq.com>
2023-01-30 23:02:12 +0800

    completed lab3

--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -1,7 +1,8 @@
 public class HorribleSteve {
     public static void main(String [] args) {
-        int i = 0;
-        for (int j = 0; i < 500; ++i, ++j) {
+        int j = 0; int i = 0;
+        for (; i < 500; ++i, ++j) {
+            System.out.println(i + " " + j + "\n");
             if (!Flik.isSameNumber(i, j)) {
                 break; // break exits the for loop!
             }
---
+++ lab3/Flik/testFlik.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class testFlik {
+    @Test
+    public void test() {
+        assertTrue(Flik.isSameNumber(128, 128));
+    }
+}
